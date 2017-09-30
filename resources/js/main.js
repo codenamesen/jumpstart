f704ec4885e9f976fdd6676cea0c29c4048447f9
codenamesen <umel@protonmail.com>
2017-09-30 11:54:00 +0530

    Creates the Carousel and corrects the bootstrap class

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 400,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false
+  });
+});
